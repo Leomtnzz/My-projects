3715f85455ff8801d042d5e144a3f193228b786c
leomtnzz <leomwetzels13@gmail.com>
2024-08-13 23:28:31 +0200

    Añado lo de devolver el cambio y lo de  las coordenadas

--- Devolver el cambio/vueltas.py
+++ Devolver el cambio/vueltas.py
@@ -1,12 +1,40 @@
-import math
+# What I want to do here is to write an amount of money and receive what would be the minimal amount of coins to pay that.
 
 D=float(input('Cuanto hay que pagar?\n'))
 print(f'Tienes que pagar ${D}')
 monedas=[2,1,0.5,0.2,0.1,0.05,0.02,0.01]
-lista=[0,0,0,0,0,0,0,0,0]
-D=math.trunc(D)
-resto=D%2
+lista=[0,0,0,0,0,0,0,0]
 
+
+l=len(monedas)
+for i in range(0,l):
+    D=round(D,2)
+    while D-monedas[i]>=0:
+        lista[i]+=1
+        D=D-monedas[i]
+#         print(round(D, 2))
+# print(lista)
+        
+l=len(lista)
+for i in range(0,l):
+    if lista[i]!=0: 
+        print(f'Necesitas {lista[i]} de {monedas[i]}€')
+            
+
+
+
+
+
+
+
+
+# import math
+
+# Un intento.
+
+# D=math.trunc(D)
+# print(D)
+# resto=D%2
 # if resto==0:
 #     A=D/2
 #     lista.append(A)
@@ -16,48 +44,3 @@ resto=D%2
 #     lista.append(A)
 #     lista.append(B)
 # print(lista)
-
-l=len(monedas)
-for i in range(0,l):
-    M=monedas-lista[i]
-    if M>lista[i]:
-        M=M-lista[i]
-        lista[i]+=1
-    else:
-        i+=1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#NO FUNCIONA
-
-# def cambio_dinero(monto):
-#     monedas=[2,1,0.5,0.2,0.1,0.05,0.02,0.01]
-#     lista=[]
-#     if (monto>=1 and monto<=99):
-#         for moneda in reversed(monedas):
-#             result=monto//moneda
-#             if result>0:
-#                 lista.append(result)
-#                 monto=monto-(moneda*result)
-#             else:
-#                 lista.append(result)
-#         return list(reversed (lista))
-#     else:
-#         return 'None'
-    
-# cambio_dinero(87)
